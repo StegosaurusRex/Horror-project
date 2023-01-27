e6b827fe113ded29ea9c95920e91f607a853eb2a
Vlad <pedogfd@gmail.com>
2023-01-28 03:06:57 +0800

    Starting room commit

--- Assets/Scripts/DoorControl.cs
+++ Assets/Scripts/DoorControl.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class DoorControl : MonoBehaviour
 {
     [SerializeField] private Animator myDoor;
-    
+    public AudioClip closeDoor;
+    public AudioClip openDoor;
+    AudioSource closeSound;
+    AudioSource openSound;
+
     private void Start()
     {
+        closeSound = gameObject.GetComponent<AudioSource>();
+        openSound = gameObject.GetComponent<AudioSource>();
         myDoor = this.transform.parent.GetComponent<Animator>();
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        closeSound.PlayOneShot(closeDoor, 1F);
+
+        // Code to execute after the delay
     }
     private void OnTriggerEnter(Collider other)
     {
         myDoor.SetBool("Open", true);
+        openSound.PlayOneShot(openDoor, 1F);
 
-        
     }
     private void OnTriggerExit(Collider other)
     {
         myDoor.SetBool("Close", true);
+        StartCoroutine(ExecuteAfterTime(1));
+
     }
 }
